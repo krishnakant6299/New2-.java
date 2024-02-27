5289ad7e042f99e10de0029035f07184697ebc8b
krishnakant6299 <154910581+krishnakant6299@users.noreply.github.com>
2024-02-27 10:36:58 +0530

    Add files via upload

---
+++ New2.java
@@ -0,0 +1,10 @@
+import java.util.concurrent.Callable;
+
+public class New2 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 3;
+        int c = a - b;
+        System.out.println(c);
+    }
+}
